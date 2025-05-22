cceb924abb12e3ca219e74c0cc71e563ad570b35
Philip Åkerfeldt <philip.akerfeldt@swace.se>
2025-05-22 23:36:50 +0200

    build: default to empty sanity variables when building for now

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -3,8 +3,8 @@ import { createClient } from 'next-sanity'
 import { apiVersion, dataset, projectId, useCdn } from '@/sanity/lib/env'
 
 export const client = createClient({
-  projectId: projectId ?? '',
-  dataset: dataset ?? '',
-  apiVersion: apiVersion ?? '2023-10-01', // use current UTC date - see "specifying API version"!
-  useCdn: useCdn ?? false // Set to false if statically generating pages, using ISR or tag-based revalidation
+  projectId,
+  dataset,
+  apiVersion,
+  useCdn // Set to false if statically generating pages, using ISR or tag-based revalidation
 })
--- src/sanity/lib/env.ts
+++ src/sanity/lib/env.ts
@@ -1,25 +1,24 @@
 import { StudioEnvironment } from '@/sanity/models/studio'
 
-export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
-)
-
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
   if (v === undefined) {
     throw new Error(errorMessage)
   }
-
+  
   return v
 }
 
 export const useCdn = false
 
 export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ?? 'testing',
   'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
 )
 
+export const dataset = assertValue(
+  process.env.NEXT_PUBLIC_SANITY_DATASET ?? 'testing',
+  'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
+)
 export const studioEnvironment = (process.env.NEXT_PUBLIC_SANITY_ENVIRONMENT ||
   'production') as StudioEnvironment
 
