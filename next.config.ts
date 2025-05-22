3e9fc3bcaf929ff07ce760490123e0b9d67e499b
Philip Åkerfeldt <philip.akerfeldt@swace.se>
2025-05-22 22:41:32 +0200

    build: update build config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: "/kpi-stats",
+  basePath: '/kpi-stats',
+  assetPrefix: '/kpi-stats',
   images: {
     unoptimized: true
   },
