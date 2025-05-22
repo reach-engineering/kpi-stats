40d1186fb34f2d1331903b877be7b7a18c622e89
Philip Åkerfeldt <philip.akerfeldt@swace.se>
2025-05-22 23:54:12 +0200

    fix: add basePath and assetPrefix

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   output: 'export',
-  // basePath: '/kpi-stats',
-  // assetPrefix: '/kpi-stats',
+  basePath: '/kpi-stats',
+  assetPrefix: '/kpi-stats',
   images: {
     unoptimized: true
   },
