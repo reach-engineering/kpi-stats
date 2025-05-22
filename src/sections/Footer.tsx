fdd6ac42f621680acbfff13bcdb7c1aef7177dda
Philip Åkerfeldt <philip.akerfeldt@swace.se>
2025-05-23 00:16:58 +0200

    feat: update footer links

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,11 +1,6 @@
 const navigation = {
   main: [
-    { name: 'About', href: '#' },
-    { name: 'Blog', href: '#' },
-    { name: 'Jobs', href: '#' },
-    { name: 'Press', href: '#' },
-    { name: 'Accessibility', href: '#' },
-    { name: 'Partners', href: '#' }
+    { name: 'About', href: '#' }
   ],
   social: [
     {
