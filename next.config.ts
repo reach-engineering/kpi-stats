8bd6ba5f89998d5265b1d956532e3bf179315cdf
Philip Åkerfeldt <philip.akerfeldt@swace.se>
2025-05-22 22:07:25 +0200

    feat: create github pages deploy config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  basePath: "/kpi-stats",
+  images: {
+    unoptimized: true
+  },
   env: {
     // Matches the behavior of `sanity dev` which sets styled-components to use the fastest way of inserting CSS rules in both dev and production. It's default behavior is to disable it in dev mode.
     SC_DISABLE_SPEEDY: 'false'
