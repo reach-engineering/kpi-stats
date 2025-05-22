3f217885daccc3871c803c721031b5f1520c6784
Philip Åkerfeldt <philip.akerfeldt@swace.se>
2025-05-22 22:58:16 +0200

    build: update packages, nextjs conf and eslint conf

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
 import { FlatCompat } from '@eslint/eslintrc'
+import tseslint from '@typescript-eslint/eslint-plugin'
+import prettier from 'prettier'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
@@ -19,8 +21,8 @@ const eslintConfig = [
   ),
   {
     plugins: {
-      '@typescript-eslint': typescriptEslint,
-      prettier
+      '@typescript-eslint': tseslint,
+      prettier: prettier
     },
     rules: {
       'max-depth': ['warn', 4],
